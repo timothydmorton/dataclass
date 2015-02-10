e323f45ba121fc188ed09f6c9c07b5f9770d4f12
Timothy Morton <tim.morton@gmail.com>
2015-02-10 10:45:17 -0500

    first code written; untested

---
+++ datastore/datastore.py
@@ -0,0 +1,116 @@
+from __future__ import print_function, division
+
+import pandas as pd
+import numpy as np
+
+
+class DataStore(object):
+    def __init__(self, data=None, **kwargs):
+        """Basic constructor.
+
+        """
+        self._maintable = 'data' 
+        self._properties = []
+
+        if data is None:
+            return
+        elif type(data)==type(''):
+            self.load_hdf(data)
+        else:
+            assert type(data) in (pd.DataFrame, pd.Series)
+
+            self.data = data
+
+            for kw,val in kwargs.items():
+                self._properties.append(kw)
+                setattr(self, kw, val)
+            
+            #require proper other tables to be defined
+            need_tables = []
+            for t in self._othertables:
+                if not hasattr(self, t):
+                    need_tables.append(t)
+
+            if need_tables != []:
+                raise ValueError('You must provide table(s): {}'.format(need_tables))
+
+    @property
+    def _othertables(self):
+        """list of names of other tables to save (besides "data")
+
+        all tables must be pandas DataFrames
+        """
+        return []
+
+    def save_hdf(self, filename, path='',
+                 tables=None, properties=None,
+                 overwrite=False, append=false):
+        """Saves to hdf5 file, under given path
+
+        all attributes are attached to the 'data' table
+        """
+
+        if os.path.exists(filename):
+            if overwrite:
+                os.remove(filename)
+            elif not append:
+                raise IOError('{} exists. Set either overwrite or append option.'.format(filename))
+
+
+        #write main datatable
+        df = getattr(self, self._maintable)
+        df.to_hdf(filename, '{}/{}'.format(path, self._maintable))
+
+        #write other attributes
+        store = pd.HDFStore(filename)
+        attrs = store.get_storer('{}/{}'.format(path,self._maintable))
+
+        if properties is None:
+            properties = {}
+        for p in self._properties:
+            properties[p] = getattr(self, p)
+        attrs.properties = properties
+
+        #save type, so that object can only be restored to proper type
+        attrs.type = type(self)
+        store.close()
+
+        #save other tables
+        for t in self._othertables:
+            df = getattr(self, t)
+            df.to_hdf(filename, '{}/{}'.format(path,t))
+
+    
+    def load_hdf(self, filename, path=''):
+        """Loads from hdf5 file, restoring correct properties to object
+        """
+
+        store = pd.HDFStore(filename)
+        attrs = store.get_storer('{}/{}'.format(path,self._maintable))
+
+        #check for property type
+        mytype = attrs.type
+        if mytype != type(self):
+            raise TypeError('Saved DataStore is {}.  Please instantiate proper class before loading.'.format(mytype))
+
+        #restore attributes
+        for kw, val in attrs.properties.items():
+            setattr(self, kw, val)
+
+        store.close()
+
+        #load main data table
+        data = pd.read_hdf(filename, '{}/{}'.format(path,self._maintable),
+                           autoclose=True)
+        setattr(self, self._maintable, data)
+
+        #load other tables, if necessary
+        for t in self._othertables:
+            df = pd.read_hdf(filename, '{}/{}'.format(path,t),
+                             autoclose=True)
+            setattr(self, t, df)
+
+        return self
+
+        
+        
---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+import os, sys
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+# Hackishly inject a constant into builtins to enable importing of the
+# package before the library is built.
+import sys
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+builtins.__datastore_SETUP__ = True
+import datastore
+version = datastore.__version__
+
+
+# Publish the library to PyPI.
+if "publish" in sys.argv[-1]:
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+# Push a new tag to GitHub.
+if "tag" in sys.argv:
+    os.system("git tag -a {0} -m 'version {0}'".format(version))
+    os.system("git push --tags")
+    sys.exit()
+
+
+setup(name = "datastore",
+      version = version,
+      description = "A generic object enabling straightforward persistance of data tables and attributes.",
+      long_description = readme(),
+      author = "Timothy D. Morton",
+      author_email = "tim.morton@gmail.com",
+      url = "https://github.com/timothydmorton/starutils",
+      packages = find_packages(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Astronomy'
+        ],
+      install_requires=['pandas>=0.13'],
+      zip_safe=False
+) 
